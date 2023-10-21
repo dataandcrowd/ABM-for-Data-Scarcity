93af48a324beaa3d621d38ef1a93813078266728
Hyesop <shinhyesop@gmail.com>
2023-10-21 08:44:02 +0100

    update RData

--- Data/AQ/openair.R
+++ Data/AQ/openair.R
@@ -27,7 +27,7 @@ ldn_sf %>%
 
 ##### KCL NO2
 ldn_no2_raw  <- importAURN(site = ldn_sf$code, pollutant = "no2", year = 2019:2022)
-ldn_no2_raw2 <- importKCL(site = ldn_sf$code, pollutant = "no2", year = 2022) 
+ldn_no2_raw2 <- importKCL(site = ldn_sf$code, pollutant = "no2", year = 2019:2022) 
 
 
 unique(ldn_no2_raw$site)
