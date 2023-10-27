67190aa19190e08915af8f3eae3463504a01d523
Hyesop <shinhyesop@gmail.com>
2023-10-27 17:02:10 +0100

    Update iterate_file_names2.R

--- Data/AQ-by-rd-stations/iterate_file_names2.R
+++ Data/AQ-by-rd-stations/iterate_file_names2.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 
 files <- list.files(pattern = "\\.csv$")
-
+files_short <- substr(files,1,3)
 
 for (i in 1:length(files)){
-  line1 <- paste0("let entirelist_", files[i], " item ticks aq_", files[i])  
-  line2 <- paste0("let aq_", files[i],"_ ", "sublist entirelist_", files[i] , " 5 29")
-  line3 <- paste0("let aq_", files[i],"__ ", "remove -999 ", "aq_", files[i],"_")
-  line4 <- paste0("ask one-of patches with [monitor-code = ", files[i], "][set no2 aq_", files[i], "__]")
+  line1 <- paste0("let entirelist_", files_short[i], " item ticks rd_", files_short[i])  
+  line2 <- paste0("let rd_", files_short[i],"_ ", "sublist entirelist_", files_short[i] , " 5 29")
+  line3 <- paste0("let rd_", files_short[i],"__ ", "remove -999 ", "rd_", files_short[i],"_")
+  line4 <- paste0("ask one-of patches with [monitor-code = ", files_short[i], "][set no2 rd_", files_short[i], "__]")
   
   print(line1)
   print(line2)
