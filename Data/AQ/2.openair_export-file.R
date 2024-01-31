3bab5470da28d1afb237a781127a2a699075b384
Hyesop <shinhyesop@gmail.com>
2024-01-31 16:30:41 +0000

    Update 2.openair_export-file.R

--- Data/AQ/2.openair_export-file.R
+++ Data/AQ/2.openair_export-file.R
@@ -9,9 +9,8 @@ ldn_image <- load("LDN_NO2.RData")
 # Station location and name
 ldn_raw
 
-ldn_sf %>% 
-  filter(site_type %in% c("Urban Background", "Suburban")) -> ldn_sf_back
-  
+ldn_sf |> filter(site_type %in% c("Urban Background", "Suburban")) -> ldn_sf_back
+ldn_sf |> filter(site_type %in% c("Kerbside", "Roadside")) -> ldn_sf_road
 
 # Function to find the nearest station excluding itself
 find_nearest_station <- function(row_index) {
@@ -30,58 +29,82 @@ ldn_sf_back
 
 
 unique(ldn_no2_raw2$site)
+ 
+ldn_no2 |>
+   arrange(code, date) |>
+   mutate(Date = as_date(date),
+          hours = as.character(hour(date)),
+          no2 = as.numeric(no2)) -> ldn_no2_clean1
 
-# ldn_no2 %>% 
-#   arrange(code, date) %>% 
-#   mutate(Date = as_date(date),
-#          hours = as.character(hour(date)),
-#          no2 = as.numeric(no2)) -> ldn_no2_clean1
-# 
-# ldn_no2_clean1 %>% 
-#   mutate(hours = as.numeric(hours),
-#          daynight = case_when(hours >= 8 & hours <= 17 ~ "Work",
-#                         TRUE ~ "Home")) -> cleaned
-# 
-# cleaned %>% 
-#   select(Date, daynight, code, site, hours, no2) %>% 
-#   mutate(hours = paste0("h", hours)) %>%
-#   pivot_wider(names_from = hours, values_from = no2, values_fill = -999) -> cleaned_wider
-# 
-# cleaned_wider %>% 
-#   group_by(code, site) %>% 
-#   mutate(id = row_number()) %>%
-#   select(id, everything()) -> cleaned_wider_with_id
-# 
-# cleaned_wider_with_id %>% 
-#   summarise(n = n()) %>% 
-#   print(n = Inf)
-# 
-# 
-# cleaned_wider_with_id %>% 
-#   summarise(n = n()) %>% 
-#   #filter(n > 500) %>% 
-#   left_join(ldn_raw, by = c("site", "code")) %>% 
-#   filter(site_type %in% c("Urban Background", "Suburban")) %>% 
-#   select(code) %>% 
-#   ungroup() -> counter
-# 
 
-# cleaned_wider_with_id %>% 
-#   inner_join(counter, by = "code") %>% 
+ ldn_no2_clean1 |>
+   mutate(hours = as.numeric(hours),
+          daynight = case_when(hours >= 8 & hours <= 17 ~ "Work",
+                         TRUE ~ "Home")) -> cleaned
+
+ cleaned |>
+   select(Date, daynight, code, site, hours, no2) |>
+   mutate(hours = paste0("h", hours)) |>
+   pivot_wider(names_from = hours, values_from = no2, values_fill = -999) -> cleaned_wider
+
+ cleaned_wider |>
+   group_by(code, site) |>
+   mutate(id = row_number()) |>
+   select(id, everything()) -> cleaned_wider_with_id
+
+ cleaned_wider_with_id |>
+   summarise(n = n()) |>
+   print(n = Inf)
+
+
+ cleaned_wider_with_id |>
+   summarise(n = n()) |>
+   #filter(n > 500) |>
+   left_join(ldn_raw, by = c("site", "code")) |>
+   filter(site_type %in% c("Urban Background", "Suburban")) |>
+   select(code) |>
+   ungroup() -> counter
+
+
+# cleaned_wider_with_id |>
+#   inner_join(counter, by = "code") |>
 #   write_csv("London_AQ_tidy.csv")
 
-
-# cleaned_wider_with_id %>% 
-#   summarise(n = n()) %>% 
-#   filter(n > 1000) %>% 
-#   left_join(ldn_raw, by = c("site", "code")) %>% 
-#   filter(site_type %in% c("Roadside", "Kerbside")) %>% 
-#   select(code) %>% 
-#   ungroup() -> counter1
+date_df <- 
+  data.frame(id = 1:2192, 
+            Date =as_date("2019-01-01"):as_date("2021-12-31") |> as_date()) |> 
+  as_tibble()
 
 
-# cleaned_wider_with_id %>% 
-#   inner_join(counter1, by = "code") %>% 
+
+cleaned_wider_with_id |>
+  summarise(n = n()) |>
+  left_join(ldn_raw, by = c("site", "code")) |>
+  filter(site_type %in% c("Roadside", "Kerbside")) |>
+  select(code) |>
+  ungroup() -> counter1
+
+
+cleaned_wider_with_id |>
+  inner_join(counter1, by = "code") |> 
+  filter(Date >= as.Date("2019-01-01") & Date <= as.Date("2021-12-31")) -> ldn_tidy_wide_rd
+
+ldn_tidy_wide_rd |> 
+  group_by(code) |> 
+  summarise(n = n()) |> 
+  filter(n > 2192)
+
+
+
+
+
+
+
+
+
+
+# cleaned_wider_with_id |>
+#   inner_join(counter1, by = "code") |>
 #   write_csv("London_AQ_tidy_rd.csv")
 
 background_stations <- c("BG1", "BG2", "BL0", "BQ7", "BX1", "BX2", "CT3", "EN1", "EN7", "GR4", "HG4", "HI0",
@@ -89,10 +112,10 @@ background_stations <- c("BG1", "BG2", "BL0", "BQ7", "BX1", "BX2", "CT3", "EN1",
 
 
 for (i in 1:length(background_stations)){
-  ldn_no2_raw2 %>% 
-    filter(code == background_stations[i]) %>% 
-    pull(no2) %>% 
-    ts() %>% 
+  ldn_no2_raw2 |> 
+    filter(code == background_stations[i]) |> 
+    pull(no2) |> 
+    ts() |> 
     ggplot_na_distribution2(title = paste0("Missing Values per Interval - ", background_stations[i]),
                             measure = "percent", 
                             number_intervals = 16, 
@@ -111,33 +134,33 @@ for (i in 1:length(background_stations)){
 
 
 
-ldn_no2_raw2 %>% 
-  group_by(site, code) %>% 
-  na_seasplit(algorithm = "kalman", find_frequency=TRUE) %>% 
+ldn_no2_raw2 |> 
+  group_by(site, code) |> 
+  na_seasplit(algorithm = "kalman", find_frequency=TRUE) |> 
   ungroup()-> aq_imputed
 
 
 unique(aq_imputed$code)
 
-ldn_no2_raw2 %>% filter(code == "HR1") %>% print(n = 200)
-aq_imputed %>% filter(code == "HR1") %>% print(n = 200)
+ldn_no2_raw2 |> filter(code == "HR1") |> print(n = 200)
+aq_imputed |> filter(code == "HR1") |> print(n = 200)
 
 
 
-aq_imputed %>% 
-  arrange(code, date) %>% 
+aq_imputed |> 
+  arrange(code, date) |> 
   mutate(Date = as_date(date),
          hours = as.character(hour(date)),
          no2 = as.numeric(no2)) -> ldn_no2_clean2
 
-ldn_no2_clean2 %>% 
+ldn_no2_clean2 |> 
   mutate(hours = as.numeric(hours),
          daynight = case_when(hours >= 8 & hours <= 17 ~ "Work",
                               TRUE ~ "Home")) -> cleaned2
 
 ########
 
-cleaned2 %>% 
+cleaned2 |> 
   ggplot(aes(date, no2, group = code)) +
   geom_line() +
   facet_wrap(code ~.)
@@ -147,66 +170,66 @@ cleaned2 %>%
 
 ########
 
-cleaned2 %>% 
-  inner_join(counter, by = "code") %>% 
-  select(Date, daynight, code, site, hours, no2) %>% 
-  mutate(hours = paste0("h", hours)) %>%
+cleaned2 |> 
+  inner_join(counter, by = "code") |> 
+  select(Date, daynight, code, site, hours, no2) |> 
+  mutate(hours = paste0("h", hours)) |>
   pivot_wider(names_from = hours, values_from = no2, values_fill = -999) -> cleaned_background
 
-cleaned_background %>% 
-  group_by(code, site) %>% 
-  mutate(id = row_number()) %>%
+cleaned_background |> 
+  group_by(code, site) |> 
+  mutate(id = row_number()) |>
   select(id, everything()) -> cleaned_background_with_id
 
-cleaned_background_with_id %>% 
-  summarise(n = n()) %>% 
+cleaned_background_with_id |> 
+  summarise(n = n()) |> 
   print(n = Inf)
 
 
-cleaned_background_with_id %>% 
-  summarise(n = n()) %>% 
-  #filter(n > 500) %>% 
-  left_join(ldn_raw, by = c("site", "code")) %>% 
-  filter(site_type %in% c("Urban Background", "Suburban")) %>% 
-  select(code) %>% 
+cleaned_background_with_id |> 
+  summarise(n = n()) |> 
+  #filter(n > 500) |> 
+  left_join(ldn_raw, by = c("site", "code")) |> 
+  filter(site_type %in% c("Urban Background", "Suburban")) |> 
+  select(code) |> 
   ungroup() -> counter2
 
 
 #Activate only when needed 
-# cleaned_background_with_id %>% 
-#   inner_join(counter2, by = "code") %>% 
+# cleaned_background_with_id |> 
+#   inner_join(counter2, by = "code") |> 
 #   write_csv("London_AQ_tidy_bg.csv")
 
 
-ldn_sf %>% 
-  inner_join(counter, by = "code") %>% 
+ldn_sf |> 
+  inner_join(counter, by = "code") |> 
   mapview() +
   mapview(ldn_bnd)
 
 
 ######
-cleaned2 %>% 
-  inner_join(counter1, by = "code") %>% 
-  select(Date, daynight, code, site, hours, no2) %>% 
-  mutate(hours = paste0("h", hours)) %>%
+cleaned2 |> 
+  inner_join(counter1, by = "code") |> 
+  select(Date, daynight, code, site, hours, no2) |> 
+  mutate(hours = paste0("h", hours)) |>
   pivot_wider(names_from = hours, values_from = no2, values_fill = -999) -> cleaned_road
 
-cleaned_road %>% 
-  group_by(code, site) %>% 
-  mutate(id = row_number()) %>%
+cleaned_road |> 
+  group_by(code, site) |> 
+  mutate(id = row_number()) |>
   select(id, everything()) -> cleaned_road_with_id
 
 
-cleaned_road_with_id %>% 
-  summarise(n = n()) %>% 
-  filter(n > 1000) %>% 
-  left_join(ldn_raw, by = c("site", "code")) %>% 
-  filter(site_type %in% c("Roadside", "Kerbside")) %>% 
-  select(code) %>% 
+cleaned_road_with_id |> 
+  summarise(n = n()) |> 
+  filter(n > 1000) |> 
+  left_join(ldn_raw, by = c("site", "code")) |> 
+  filter(site_type %in% c("Roadside", "Kerbside")) |> 
+  select(code) |> 
   ungroup() -> counter3
 
 
 
-# cleaned_wider_with_id %>% 
-#   inner_join(counter3, by = "code") %>% 
+# cleaned_wider_with_id |> 
+#   inner_join(counter3, by = "code") |> 
 #   write_csv("London_AQ_tidy_rd.csv")
