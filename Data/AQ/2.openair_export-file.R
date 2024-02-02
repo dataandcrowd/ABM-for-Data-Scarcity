c70d4fe31fca27beb7bd61990500638e058cd2b2
Hyesop <shinhyesop@gmail.com>
2024-02-02 14:47:35 +0000

    update roadside data from statistical imputation

--- Data/AQ/2.openair_export-file.R
+++ Data/AQ/2.openair_export-file.R
@@ -42,194 +42,106 @@ ldn_no2 |>
           daynight = case_when(hours >= 8 & hours <= 17 ~ "Work",
                          TRUE ~ "Home")) -> cleaned
 
- cleaned |>
+cleaned |> 
+   left_join(ldn_raw, by = c("site", "code")) |>
+   filter(site_type %in% c("Roadside", "Kerbside")) |> 
+  select(-c(latitude, longitude, source)) -> road_data
+ 
+
+
+road_data |> 
+  select(Date, code) |> 
+  group_by(code) |> 
+  summarise(n = n(), .groups = "drop")
+
+
+road_data |> 
+  select(Date, code) |> 
+  group_by(code) |> 
+  summarise(n = n(), .groups = "drop") |> 
+  filter(n > 20000) |> 
+  pull(code) -> stations_with_sufficient_data
+
+
+ 
+# Define start and end dates
+start_date <- ymd_hms("2019-01-01 00:00:00")
+end_date <- ymd_hms("2021-12-31 23:00:00")
+
+# Create a sequence of hourly timestamps
+hourly_sequence <- seq(from = start_date, to = end_date, by = "hour")
+
+# Create a data frame
+hourly_data_frame <- tibble(timestamp = hourly_sequence)
+
+ 
+ 
+road_data |> 
+  filter(code %in% stations_with_sufficient_data) -> road_data_station_filtered
+
+
+#############################################
+
+road_data_station_filtered |> 
+  rename(timestamp = date) |> 
+  group_by(code) |> 
+  group_split() -> list_of_road
+
+merged_list <- map(list_of_road, ~right_join(.x, hourly_data_frame, by = "timestamp"))
+
+############################################
+
+process_dataframe <- function(df) {
+  df |> 
+    fill(site, code, site_type, .direction = "downup") |> 
+    mutate(Date = as_date(timestamp),
+           hours = hour(timestamp),
+           daynight = case_when(hours >= 8 & hours <= 17 ~ "Work",
+                                TRUE ~ "Home")) |> 
+    arrange(site, code, timestamp) |> 
+    group_by(site, code) |> 
+    na_seasplit(algorithm = "kalman", find_frequency=TRUE) |> 
+    ungroup()
+}
+
+list_of_dfs <- lapply(merged_list, process_dataframe)
+
+na_counts <- function(imsi){
+  imsi %>% 
+    group_by(code) %>% 
+    summarise(na_count = sum(is.na(no2)))
+  
+}
+
+lapply(list_of_dfs, na_counts) |> bind_rows()
+
+
+aq_imputed <- list_of_dfs |> 
+  bind_rows() |> 
+  mutate(Date = as_date(timestamp))
+
+###############################################
+
+aq_imputed |>
    select(Date, daynight, code, site, hours, no2) |>
    mutate(hours = paste0("h", hours)) |>
-   pivot_wider(names_from = hours, values_from = no2, values_fill = -999) -> cleaned_wider
+   pivot_wider(names_from = hours, values_from = no2, values_fill = -999) -> road_wider
 
- cleaned_wider |>
+
+road_wider |>
    group_by(code, site) |>
    mutate(id = row_number()) |>
-   select(id, everything()) -> cleaned_wider_with_id
+   select(id, everything()) -> road_wider_with_id
 
- cleaned_wider_with_id |>
+road_wider_with_id |>
    summarise(n = n()) |>
    print(n = Inf)
 
 
- cleaned_wider_with_id |>
-   summarise(n = n()) |>
-   #filter(n > 500) |>
-   left_join(ldn_raw, by = c("site", "code")) |>
-   filter(site_type %in% c("Urban Background", "Suburban")) |>
-   select(code) |>
-   ungroup() -> counter
 
-
-# cleaned_wider_with_id |>
-#   inner_join(counter, by = "code") |>
-#   write_csv("London_AQ_tidy.csv")
-
-date_df <- 
-  data.frame(id = 1:2192, 
-            Date =as_date("2019-01-01"):as_date("2021-12-31") |> as_date()) |> 
-  as_tibble()
-
-
-
-cleaned_wider_with_id |>
-  summarise(n = n()) |>
-  left_join(ldn_raw, by = c("site", "code")) |>
-  filter(site_type %in% c("Roadside", "Kerbside")) |>
-  select(code) |>
-  ungroup() -> counter1
-
-
-cleaned_wider_with_id |>
-  inner_join(counter1, by = "code") |> 
-  filter(Date >= as.Date("2019-01-01") & Date <= as.Date("2021-12-31")) -> ldn_tidy_wide_rd
-
-ldn_tidy_wide_rd |> 
-  group_by(code) |> 
-  summarise(n = n()) |> 
-  filter(n > 2192)
+road_wider_with_id |> 
+  write_csv("London_AQ_tidy_rd.csv")
 
 
 
 
-
-
-
-
-
-
-# cleaned_wider_with_id |>
-#   inner_join(counter1, by = "code") |>
-#   write_csv("London_AQ_tidy_rd.csv")
-
-background_stations <- c("BG1", "BG2", "BL0", "BQ7", "BX1", "BX2", "CT3", "EN1", "EN7", "GR4", "HG4", "HI0",
-"HR1", "IS6", "KC1", "LB6", "LH0", "LW1", "LW5", "NM3",  "RB7", "RI2", "SK6", "WA2", "WA9", "WM0")
-
-
-for (i in 1:length(background_stations)){
-  ldn_no2_raw2 |> 
-    filter(code == background_stations[i]) |> 
-    pull(no2) |> 
-    ts() |> 
-    ggplot_na_distribution2(title = paste0("Missing Values per Interval - ", background_stations[i]),
-                            measure = "percent", 
-                            number_intervals = 16, 
-                            xlab = "Time Lapse - Interval Size: Approx 3 months",
-                            color_missing = "gold3")
-  
-  # Define the filename
-  file_name <- paste0("NA_Distribution/", background_stations[i], ".jpg")
-  
-  # Check if the file exists
-  if (!file.exists(file_name)){
-    ggsave(filename = file_name, width = 4.5, height = 4)
-  }
-  
-}
-
-
-
-ldn_no2_raw2 |> 
-  group_by(site, code) |> 
-  na_seasplit(algorithm = "kalman", find_frequency=TRUE) |> 
-  ungroup()-> aq_imputed
-
-
-unique(aq_imputed$code)
-
-ldn_no2_raw2 |> filter(code == "HR1") |> print(n = 200)
-aq_imputed |> filter(code == "HR1") |> print(n = 200)
-
-
-
-aq_imputed |> 
-  arrange(code, date) |> 
-  mutate(Date = as_date(date),
-         hours = as.character(hour(date)),
-         no2 = as.numeric(no2)) -> ldn_no2_clean2
-
-ldn_no2_clean2 |> 
-  mutate(hours = as.numeric(hours),
-         daynight = case_when(hours >= 8 & hours <= 17 ~ "Work",
-                              TRUE ~ "Home")) -> cleaned2
-
-########
-
-cleaned2 |> 
-  ggplot(aes(date, no2, group = code)) +
-  geom_line() +
-  facet_wrap(code ~.)
-
-
-
-
-########
-
-cleaned2 |> 
-  inner_join(counter, by = "code") |> 
-  select(Date, daynight, code, site, hours, no2) |> 
-  mutate(hours = paste0("h", hours)) |>
-  pivot_wider(names_from = hours, values_from = no2, values_fill = -999) -> cleaned_background
-
-cleaned_background |> 
-  group_by(code, site) |> 
-  mutate(id = row_number()) |>
-  select(id, everything()) -> cleaned_background_with_id
-
-cleaned_background_with_id |> 
-  summarise(n = n()) |> 
-  print(n = Inf)
-
-
-cleaned_background_with_id |> 
-  summarise(n = n()) |> 
-  #filter(n > 500) |> 
-  left_join(ldn_raw, by = c("site", "code")) |> 
-  filter(site_type %in% c("Urban Background", "Suburban")) |> 
-  select(code) |> 
-  ungroup() -> counter2
-
-
-#Activate only when needed 
-# cleaned_background_with_id |> 
-#   inner_join(counter2, by = "code") |> 
-#   write_csv("London_AQ_tidy_bg.csv")
-
-
-ldn_sf |> 
-  inner_join(counter, by = "code") |> 
-  mapview() +
-  mapview(ldn_bnd)
-
-
-######
-cleaned2 |> 
-  inner_join(counter1, by = "code") |> 
-  select(Date, daynight, code, site, hours, no2) |> 
-  mutate(hours = paste0("h", hours)) |>
-  pivot_wider(names_from = hours, values_from = no2, values_fill = -999) -> cleaned_road
-
-cleaned_road |> 
-  group_by(code, site) |> 
-  mutate(id = row_number()) |>
-  select(id, everything()) -> cleaned_road_with_id
-
-
-cleaned_road_with_id |> 
-  summarise(n = n()) |> 
-  filter(n > 1000) |> 
-  left_join(ldn_raw, by = c("site", "code")) |> 
-  filter(site_type %in% c("Roadside", "Kerbside")) |> 
-  select(code) |> 
-  ungroup() -> counter3
-
-
-
-# cleaned_wider_with_id |> 
-#   inner_join(counter3, by = "code") |> 
-#   write_csv("London_AQ_tidy_rd.csv")
